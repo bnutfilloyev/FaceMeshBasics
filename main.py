7ab69427e45ca8bc12293dceda0d378a2b14e4e6
Bexruz <yoshlikmedia@gmail.com>
2021-05-02 14:49:15 +0500

    Add main.py

---
+++ main.py
@@ -0,0 +1,33 @@
+import cv2
+import mediapipe as mp
+import time
+
+cap = cv2.VideoCapture(1)
+
+cTime = 0
+pTime = 0
+
+mpDraw = mp.solutions.drawing_utils
+mpFaceMesh = mp.solutions.face_mesh
+faceMesh = mpFaceMesh.FaceMesh(max_num_faces=100000)
+drawSpec = mpDraw.DrawingSpec(thickness=1, circle_radius=2)
+
+while True:
+    success, img = cap.read()
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+    results = faceMesh.process(imgRGB)
+    if results.multi_face_landmarks:
+        for faceLms in results.multi_face_landmarks:
+            mpDraw.draw_landmarks(img, faceLms, mpFaceMesh.FACE_CONNECTIONS, drawSpec, drawSpec)
+
+
+
+    ### FPS
+    cTime = time.time()
+    fps = 1/(cTime - pTime)
+    pTime = cTime
+
+    cv2.putText(img, f"FPS:{int(fps)}", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 255), 3)
+
+    cv2.imshow("output", img)
+    cv2.waitKey(1)
