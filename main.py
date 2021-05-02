e990c441fcf6b18d61201608123ff0bc9a6e7912
Bexruz <yoshlikmedia@gmail.com>
2021-05-02 14:50:40 +0500

    Add commit in main.py

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ import cv2
 import mediapipe as mp
 import time
 
+# init cam
 cap = cv2.VideoCapture(1)
 
 cTime = 0
 pTime = 0
 
+# Draw face
 mpDraw = mp.solutions.drawing_utils
 mpFaceMesh = mp.solutions.face_mesh
 faceMesh = mpFaceMesh.FaceMesh(max_num_faces=100000)
@@ -27,7 +29,9 @@ while True:
     fps = 1/(cTime - pTime)
     pTime = cTime
 
+    #FPS text
     cv2.putText(img, f"FPS:{int(fps)}", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 255), 3)
 
+    #Camera show
     cv2.imshow("output", img)
     cv2.waitKey(1)
